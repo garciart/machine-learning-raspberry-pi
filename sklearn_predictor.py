d3097bd89121bbe8408345879b14e8be757b291a
garciart <rgarcia@rgprogramming.com>
2020-02-19 11:46:27 -0500

    Raspberry Pi test.

--- sklearn_predictor.py
+++ sklearn_predictor.py
@@ -28,6 +28,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 # Module metadata dunders
 __author__ = "Rob Garcia"
@@ -60,17 +61,18 @@ X_train, X_validation, Y_train, Y_validation = train_test_split(
 
 # Spot Check Algorithms
 models = []
-models.append(("LR", LogisticRegression(
+models.append(("Logistic Regression", LogisticRegression(
     solver="liblinear", multi_class="ovr")))
-models.append(("LDA", LinearDiscriminantAnalysis()))
-models.append(("KNN", KNeighborsClassifier()))
-models.append(("CART", DecisionTreeClassifier()))
-models.append(("NB", GaussianNB()))
-models.append(("SVM", SVC(gamma="auto")))
+models.append(("Linear Discriminant Analysis (LDA)", LinearDiscriminantAnalysis()))
+models.append(("k-Nearest Neighbors Classifier (k-NN)", KNeighborsClassifier()))
+models.append(("Decision Tree Classifier", DecisionTreeClassifier()))
+models.append(("Gaussian Naive Bayes (GaussianNB)", GaussianNB()))
+models.append(("RandomForestClassifier", RandomForestClassifier()))
+models.append(("Support Vector Machine (SVM)", SVC(gamma="auto")))
 # evaluate each model in turn
 results = []
 names = []
-print("Models and accuracy estimations:")
+print("Models and accuracy estimations (mean, standard deviation):")
 for name, model in models:
     kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
     cv_results = cross_val_score(
@@ -79,17 +81,30 @@ for name, model in models:
     names.append(name)
     print("%s: %f (%f)" % (name, cv_results.mean(), cv_results.std()))
 
+print()
+
+# Slightly Warm, Slightly Cool, Neutral
+observation = [[[0.1, 50.0, 1.0, 0.5, 23.0]], [[0.1, 60.0, 1.0, 0.6, 26.0]], [[0.1, 76.0, 1.0, 0.6, 28.0]]]
+for o in observation:
+    print("Sample data:", o)
+
+print("\nUsing Logistic Regression...")
+clf = LogisticRegression(solver="liblinear", multi_class="ovr")
+model = clf.fit(X, y)
+for o in observation:
+    print("Prediction:", model.predict(o))
+    # print(model.predict_proba(o))
+
 print("\nUsing Decision Tree Classifier...")
-observation = [[0.1, 76.0, 1.0, 0.6, 28.0]]
-print("Sample data:", observation)
 clf = DecisionTreeClassifier(criterion='gini', random_state=0)
 model = clf.fit(X, y)
-print("Prediction:", model.predict(observation))
-# print(model.predict_proba(observation))
+for o in observation:
+    print("Prediction:", model.predict(o))
+    # print(model.predict_proba(o))
 
 print("\nUsing Support Vector Machine...")
-observation = [[0.1, 50.0, 1.0, 0.5, 23.0]]
-print("Sample data:", observation)
 clf = SVC(kernel='linear', C = 1.0)
-clf.fit(X, y)
-print("Prediction:", clf.predict(observation))
+model = clf.fit(X, y)
+for o in observation:
+    print("Prediction:", model.predict(o))
+    # print(model.predict_proba(o))
