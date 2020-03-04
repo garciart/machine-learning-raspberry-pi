a9e33dda0c0b836a8308c5e0ee585c2b3f45ad06
garciart <rgarcia@rgprogramming.com>
2020-03-04 17:41:30 -0500

    Stand-alone sensor complete. OTA Socket test successful.

--- smart_sensor.py
+++ smart_sensor.py
@@ -47,13 +47,47 @@ DHT_SENSOR_PORT = 7  # Digital port 7
 ON = 1
 OFF = 0
 
+def sensors_test():
+    """Test the GrovePi DHT sensor, LEDs and RGB LCD screen."""
+    try:
+        for y in range(3):
+            # Get sensor data and actuate LEDs and RGB LCD screen
+            [temp, humid] = grovepi.dht(DHT_SENSOR_PORT, BLUE_DHT)
+            if not math.isnan(temp) and not math.isnan(humid):
+                if temp >= 20.0:
+                    # Temperature is good: Everything is green.
+                    grove_rgb_lcd.setRGB(0, 255, 0)
+                    grovepi.digitalWrite(GREEN_LED, ON)
+                    time.sleep(2)
+                    grovepi.digitalWrite(GREEN_LED, OFF)
+                else:
+                    # Temperature is too low: Turn to red.
+                    grove_rgb_lcd.setRGB(255, 0, 0)
+                    grovepi.digitalWrite(RED_LED, ON)
+                    time.sleep(2)
+                    grovepi.digitalWrite(RED_LED, OFF)
+                t_str = str(temp)
+                h_str = str(humid)
+                print("Temperature: {}C | Humidity: {}%".format(t_str, h_str))
+                grove_rgb_lcd.setText_norefresh("Tmp: {} C\nHmd: {} %".format(t_str, h_str))
+            # For DHT22, wait three seconds before next reading
+            time.sleep(3)
+            grove_rgb_lcd.setRGB(0, 0, 0)
+    except (IOError, TypeError) as ex:
+        print("Error: {}".format(str(ex)))
+        shutdown_board()
+    except KeyboardInterrupt:
+        shutdown_board()
+    shutdown_board()
+
+
 def prepare_model(file_name, label_names):
     """Train, test, and run different classification estimators and select
     the best classifier.
     (see
     https://scikit-learn.org/stable/tutorial/machine_learning_map/index.html
     for a list of classifiers)
-    
+
     :param file_name: The name of the csv file with the data.
     :type file_name: str
     :param label_names: The list of labels.
@@ -61,7 +95,6 @@ def prepare_model(file_name, label_names):
     :return: The most accurate classifer model in the form of a history object.
     :rtype: object
     """
-    print("Training and testing model...\n")
     # Limit decimal places to three and do not use scientific notation
     np.set_printoptions(precision=3)
     np.set_printoptions(suppress=True)
@@ -125,28 +158,55 @@ def prepare_model(file_name, label_names):
     # Evaluate the accuracy of each estimator (limited to classifiers)
     results = []
     for name, classifier in estimators:
-        model = classifier.fit(x_train, y_train)
-        prediction = model.predict(x_validation)
+        training_model = classifier.fit(x_train, y_train)
+        prediction = training_model.predict(x_validation)
         score = accuracy_score(y_validation, prediction)
-        results.append((name, model, score))
+        results.append((name, training_model, score))
 
     # Get and sort the results
     results.sort(key=lambda r: r[2], reverse=True)
 
+    # Select the highest scoring training model and refit to the entire dataset
     top_model_name = results[0][0]
-    # model = classifier.fit(feature_values, label_values)
-    top_full_model = results[0][1].fit(feature_values, label_values)
+    top_model_refit = results[0][1].fit(feature_values, label_values)
     top_model_score = results[0][2]
-    
-    # Select the highest scoring classification model
-    selected_model = [top_model_name, top_full_model, top_model_score]
-    
+
+    # Package and return
+    selected_model = [top_model_name, top_model_refit, top_model_score]
     return selected_model
 
 
-def get_sensor_data():
-    """Collects temperature and humidity data"""
-    print("\nCollecting sensor data...")
+def check_model(label_names, selected_model):
+    """Check selected model against unlabeled data (with expected predictions).
+
+    :param label_names: The list of labels.
+    :type label_names: list
+    :param selected_model: The most accurate classifer model in the form of a history object.
+    :type selected_model: object
+    """
+    print("Data to be evaluated:")
+    unlabeled_x = []
+    unlabeled_x.append(([[1013.25, 0.1, 50.0, 1.0, 0.61, 23.0]], "Slightly Cool"))
+    unlabeled_x.append(([[1013.25, 0.1, 60.0, 1.0, 0.61, 26.0]], "Neutral"))
+    unlabeled_x.append(([[1013.25, 0.1, 76.0, 1.0, 0.61, 28.0]], "Slightly Warm"))
+    # expected_y = ["Slightly Cool", "Neutral", "Slightly Warm"]
+
+    for i, (data, expected_label) in enumerate(unlabeled_x, start=1):
+        print("Sample #{}: {} = {}".format(i, data, expected_label))
+
+    # Run unlabeled data against selected model and check accuracy
+    print("Prediction(s):")
+    for j, (data, expected_label) in enumerate(unlabeled_x, start=1):
+        print("Sample #{}: Prediction: {} (expected {})".format(
+            j, label_names[int(selected_model[1].predict(data))], expected_label))
+
+
+def collect_sensor_data():
+    """Collects temperature and humidity data
+    :return: The temperature and humidity data collected, formated with
+             atmospheric pressure, air speed, metabolic rate, and clothing level.
+    :rtype: list of tuples
+    """
     sensor_data = []
     try:
         for y in range(3):
@@ -154,69 +214,63 @@ def get_sensor_data():
             if not math.isnan(temp) and not math.isnan(humid):
                 t_str = str(temp)
                 h_str = str(humid)
-                print("Temperature: {}C | Humidity: {}%".format(t_str, h_str))
-                grove_rgb_lcd.setText_norefresh("Temp: {} C\nHumidity: {} %".format(t_str, h_str))
-                sensor_data.append([1013.25, 0.1, humid, 1.0, 0.61, temp])
+                grove_rgb_lcd.setRGB(127, 127, 127)
+                grove_rgb_lcd.setText_norefresh("Tmp: {} C\nHmd: {} %".format(t_str, h_str))
+                sensor_data.append(([[1013.25, 0.1, humid, 1.0, 0.61, temp]]))
             # For DHT22, wait three seconds before next reading
             time.sleep(3)
+            grove_rgb_lcd.setRGB(0, 0, 0)
     except (IOError, TypeError) as ex:
         print("Error: {}".format(str(ex)))
         shutdown_board()
     except KeyboardInterrupt:
         shutdown_board()
+    for index, data in enumerate(sensor_data):
+        print("Sample #{0} collected: {1}".format(index + 1, data))
+    shutdown_board()
     return sensor_data
 
 
-def process_data():
-    """Train, test, and run different classification estimators, selected from
-    https://scikit-learn.org/stable/tutorial/machine_learning_map/index.html
-    
-    :param unlabeled_x: Unlabeled data to be classified.
-    :type unlabeled_x: list of tuples
-    :param expected_y: The expected results of classifying the unlabeled data.
-    :type expected_y: list
-    :return: The trained model in the form of a history object.
-    :rtype: object
+def process_sensor_data(label_names, selected_model, sensor_data):
+    """Classify unlabeled sensor data against the selected model and get a prediction.
+
+    :param label_names: The list of labels.
+    :type label_names: list
+    :param selected_model: The most accurate classifer model in the form of a history object.
+    :type selected_model: object
+    :param sensor_data: The temperature and humidity data collected, formated with
+                        atmospheric pressure, air speed, metabolic rate, and clothing level.
+    :type sensor_data: list of tuples
     """
-    pass
-
-def sensors_only_test():
-    """Collects and displays temperature and humidity data"""
-    sensor_data = []
-
-    for x in range(10):
-        try:
-            for y in range(3):
-                [temp, humid] = grovepi.dht(DHT_SENSOR_PORT, BLUE_DHT)
-                if not math.isnan(temp) and not math.isnan(humid):
-                    if temp >= 20.0:
-                        # Temperature is good: Everything is green.
-                        grove_rgb_lcd.setRGB(0, 255, 0)
-                        grovepi.digitalWrite(GREEN_LED, ON)
-                        time.sleep(2)
-                        grovepi.digitalWrite(GREEN_LED, OFF)
-                    else:
-                        # Temperature is too low: Turn to red.
-                        grove_rgb_lcd.setRGB(255, 0, 0)
-                        grovepi.digitalWrite(RED_LED, ON)
-                        time.sleep(2)
-                        grovepi.digitalWrite(RED_LED, OFF)
-                    t_str = str(temp)
-                    h_str = str(humid)
-                    print("Temperature: {}C | Humidity: {}%".format(t_str, h_str))
-                    grove_rgb_lcd.setText_norefresh("Temp: {} C\nHumidity: {} %".format(t_str, h_str))
-                    sensor_data.append([temp, humid])
-                # For DHT22, wait three seconds before next reading
-                time.sleep(3)
-        except (IOError, TypeError) as ex:
-            print("Error: {}".format(str(ex)))
-            shutdown_board()
-        except KeyboardInterrupt:
-            shutdown_board()
-        print(sensor_data)
-        # Wait 30 seconds before collecting next set of data
-        grove_rgb_lcd.setRGB(0, 0, 0)
-        time.sleep(30)
+    try:
+        sensation = 0
+        for j, data in enumerate(sensor_data, start=1):
+            print("Sensor data #{}: Prediction: {}".format(
+            j, label_names[int(selected_model[1].predict(data))]))
+            sensation += int(selected_model[1].predict(data))
+        sensation = int(sensation / len(sensor_data))
+        print("Overall sensation: {} ({})".format(sensation, label_names[sensation]))
+        grove_rgb_lcd.setText_norefresh("Sensation:\n{} ({})".format(sensation, label_names[sensation]))
+        if sensation == 3:
+            # Temperature is good: Everything is green.
+            grove_rgb_lcd.setRGB(0, 255, 0)
+            grovepi.digitalWrite(GREEN_LED, ON)
+            time.sleep(2)
+            grovepi.digitalWrite(GREEN_LED, OFF)
+            grove_rgb_lcd.setRGB(0, 0, 0)
+            grove_rgb_lcd.setText_norefresh("")
+        else:
+            # Temperature is too low: Turn to red.
+            grove_rgb_lcd.setRGB(255, 0, 0)
+            grovepi.digitalWrite(RED_LED, ON)
+            time.sleep(2)
+            grovepi.digitalWrite(RED_LED, OFF)
+    except (IOError, TypeError) as ex:
+        print("Error: {}".format(str(ex)))
+        shutdown_board()
+    except KeyboardInterrupt:
+        shutdown_board()
+    time.sleep(3)
     shutdown_board()
 
 
@@ -226,47 +280,42 @@ def shutdown_board():
     grovepi.digitalWrite(GREEN_LED, OFF)
     grove_rgb_lcd.setRGB(0, 0, 0)
     grove_rgb_lcd.setText_norefresh("")
-    print("Job complete. Have an excellent day.")
 
 
 def main():
     """Application entry point."""
-    start_time = time.time()
-    print("Test of the GrovePi DHT sensor, LEDs and RGB LCD screen.\n")
+    print("Smart Sensor Application.\n")
+
+    # Test sensors first (optional)
+    print("Testing sensors...")
+    sensors_test()
+    print("Test complete.\n")
+
     file_name = "thermal_comfort.csv"
     label_names = ["Cold", "Cool", "Slightly Cool",
                    "Neutral", "Slightly Warm", "Warm", "Hot"]
+    start_time = time.time()
+
+    print("Training and testing model...")
     selected_model = prepare_model(file_name, label_names)
-    print("Model selected: {0} ({1:.2f}%).\n".format(selected_model[0], selected_model[2]))
-    print(selected_model)
-    
-    
-    unlabeled_x = []
-    unlabeled_x.append(([[1013.25, 0.1, 50.0, 1.0, 0.61, 23.0]], "Slightly Cool"))
-    unlabeled_x.append(([[1013.25, 0.1, 60.0, 1.0, 0.61, 26.0]], "Neutral"))
-    unlabeled_x.append(([[1013.25, 0.1, 76.0, 1.0, 0.61, 28.0]], "Slightly Warm"))
-    # expected_y = ["Slightly Cool", "Neutral", "Slightly Warm"]
-    
-    print("Data to be evaluated:")
-    for i, (data, expected_label) in enumerate(unlabeled_x, start=1):
-        print("Sample #{}: {} = {}".format(i, data, expected_label))
+    print("Model selected: {0} ({1:.2f}%).".format(selected_model[0], selected_model[2]))
+    print("Training and testing model complete.")
+    print("Elapsed time: {} seconds.\n".format((time.time() - start_time)))
+
+    print("Checking model against unlabeled data...")
+    check_model(label_names, selected_model)
     print()
 
-    # Run samples using all classifiers in accuracy order
-    for j, (data, expected_label) in enumerate(unlabeled_x, start=1):
-        print("Sample #{}: Prediction: {} (expected {})".format(
-            j, label_names[int(selected_model[1].predict(data))], expected_label))
+    print("Collecting sensor data...")
+    sensor_data = collect_sensor_data()
+    print("Collection complete.\n")
+
+    print("Processing sensor data...")
+    process_sensor_data(label_names, selected_model, sensor_data)
     print()
-    
-    sensor_data = get_sensor_data()
-    print(sensor_data)
-    
-    for j, data in enumerate(sensor_data, start=1):
-        print("Sensor data #{}: Prediction: {})".format(
-        j, label_names[int(selected_model[1].predict(data))]))
-    print()
-    
-    print("Elapsed time: {} seconds.".format((time.time() - start_time)))
+
+    print("Shutting down board...")
+    shutdown_board()
     print("Job complete. Have an excellent day.")
 
 
---
+++ smart_sensor_client.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+TCP Socket Client Test
+https://stackoverflow.com/questions/34548037/two-raspberries-communicating-each-other
+usage: sudo ./sock_test_client.py
+Python version used: 3.6.8
+See requirements.txt for additional dependencies
+Styling guide: PEP 8 -- Style Guide for Python Code
+    (https://www.python.org/dev/peps/pep-0008/) and
+    PEP 257 -- Docstring Conventions
+    (https://www.python.org/dev/peps/pep-0257/)
+"""
+
+import socket
+import time
+
+# Module metadata dunders
+__author__ = "Rob Garcia"
+__copyright__ = "Copyright 2019-2020, Rob Garcia"
+__email__ = "rgarcia@rgprogramming.com"
+__license__ = "MIT"
+
+# Waveshare Sense HAT (B) information
+HOST = "192.168.1.12"
+PORT = 333
+
+
+
+def main():
+    """Application entry point."""
+    for i in range(3):
+        print("Sending data...")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            if i < 2:
+                data = b"Hello, friend."
+                delay = 10
+            else:
+                data = b"Good-bye!"
+                delay = 0
+            s.sendall(data)
+            data = s.recv(1024)
+            print("Received {} from server!".format(repr(data.decode("ascii"))))
+            time.sleep(delay)
+    print("Signing off: Good-bye.")
+
+
+if __name__ == "__main__":
+    main()
