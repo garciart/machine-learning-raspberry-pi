2de6aab43e2928446355182b7c138ea37cce4c2d
garciart <rgarcia@rgprogramming.com>
2020-03-13 21:19:09 -0400

    Upload to fix requirements.txt

--- permissions.sh
+++ permissions.sh
@@ -5,9 +5,9 @@ chmod +x ml_scripts/sl_example.py
 chmod +x ml_scripts/tf_example.py
 chmod +x ml_scripts/tf_premade.py
 chmod +x ml_scripts/thermal_calc.py
-chmod +x s3/sensor_test.py
-chmod +x s3/smart_sensor.py
-chmod +x s3/smart_sensor_client.py
-chmod +x s3/smart_sensor_server.py
-chmod +x s3/sock_test_client.py
-chmod +x s3/sock_test_server.py
+chmod +x s3_scripts/sensor_test.py
+chmod +x s3_scripts/smart_sensor.py
+chmod +x s3_scripts/smart_sensor_client.py
+chmod +x s3_scripts/smart_sensor_server.py
+chmod +x s3_scripts/sock_test_client.py
+chmod +x s3_scripts/sock_test_server.py
